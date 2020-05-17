86d9829e1a5b87f1279d653efc70a2943700dabe
claby2 <wibow9770@gmail.com>
2020-05-17 19:20:23 +0800

    split lampturn prefab into two variants, flicker and no flicker

---
+++ Assets/LampturnBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine;
+
+public class LampturnBehavior : MonoBehaviour {
+
+    public Light2D Light;
+
+    private float lightStepChange = 0.5f;
+    private float lightStep = 0.5f;
+
+    void Update() {
+
+        Light.intensity -= lightStep * Time.deltaTime;
+
+        if(Light.intensity < 0.6f) {
+            lightStep = -lightStepChange;
+        } else if(Light.intensity > 1.0f) {
+            lightStep = lightStepChange;
+        }
+        
+    }
+}
