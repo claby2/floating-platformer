bb9ec3b6acf74ab94edc374c8ecc9efd967a686f
claby2 <wibow9770@gmail.com>
2020-05-17 12:12:52 +0800

    added cursor follow point light

---
+++ Assets/CursorFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorFollow : MonoBehaviour {
+    public Camera MainCamera;
+
+    void Start() {
+        Cursor.visible = false;
+    }
+
+    void Update() {
+        Vector2 cursorPos = MainCamera.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+    }
+}
