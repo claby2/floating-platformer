0a6ea6fe801a11886da34beabb69ab24b7e52bbc
claby2 <wibow9770@gmail.com>
2020-05-18 19:36:48 +0800

    added trigger to restart game when player falls

---
+++ Assets/FallTriggerBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FallTriggerBehavior : MonoBehaviour {
+
+    public GameObject Player;
+    public float yDifference = 50f;
+
+    private PlayerControl playerControl;
+
+    void Start() {        
+        playerControl = Player.GetComponent<PlayerControl>();
+    }
+
+    void FixedUpdate() {
+        Vector3 tempPosition = transform.position;
+        tempPosition.x = Player.transform.position.x;
+        if(playerControl.isGrounded) {
+            tempPosition.y = Player.transform.position.y - yDifference;
+        }
+        transform.position = tempPosition;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.collider.CompareTag("Player")) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+    }
+}
